68b6e3fb161ce32fd3fedf67023a94ee6c92883c
lomic28 <try7loving@gmail.com>
2018-05-26 03:36:49 +0800

    custom theme: disqus

---
+++ docs/.vuepress/theme/Disqus.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content disqus">
+    <div id="disqus_thread"></div>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript> 
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    var disqus_config = function () {
+      this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+      this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    var d = document, s = d.createElement('script');
+    s.src = 'https://wenkers-ourbook.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    d.querySelector('.disqus').appendChild(s);
+  }
+}
+</script>
+
+<style lang="stylus">
+.content.disqus
+  padding: 0 !important
+  > *:first-child
+    margin-top: 0 !important
+</style>
